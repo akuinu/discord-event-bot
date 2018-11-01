90fc3fcb6a74b46ebbc870eb1bfd6025015962de
akuinu <akuinumail@gmail.com>
2018-11-01 12:15:33 +0200

    trying to fix db connection.

--- dbObjects.js
+++ dbObjects.js
@@ -3,8 +3,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:  'postgres',
     protocol: 'postgres',
-    port:     match[4],
-    host:     match[3],
+    port:     5432,
     logging:  true //false
   });
 
