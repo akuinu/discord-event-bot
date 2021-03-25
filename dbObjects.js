13662fe099278c41d1ba95b87ada55bb323d2cb8
akuinu <39266030+akuinu@users.noreply.github.com>
2021-03-25 21:45:21 +0200

    Update dbObjects.js

--- dbObjects.js
+++ dbObjects.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL,{logging: false});
+const sequelize = new Sequelize(process.env.DATABASE_URL,{logging: false, ssl: true});
 
 const Servers = require('./models/Servers')(sequelize, Sequelize.DataTypes)
 const Types = require('./models/Types')(sequelize, Sequelize.DataTypes)
